63626ce05f27a9e68cda6449bec82c0f02a4c5d0
goldyfruit <gaetan.trellu@incloudus.com>
2016-02-16 19:33:22 -0500

    [color] Add color.py file to be able to colorize the ouput

---
+++ uoi/color.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import sys
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+# Following from Python cookbook, #475186
+def use_color(stream):
+    if not hasattr(stream, "isatty"):
+        return False
+    if not stream.isatty():
+        # Auto color only on TTYs
+        return False
+    try:
+        import curses
+        curses.setupterm()
+        return curses.tigetnum("colors") > 2
+    except:
+        # Guess false in case of error
+        return False
+
+def printc(text, color=WHITE):
+    if use_color:
+        seq = "\x1b[1;%dm" % (30+color) + text + "\x1b[0m"
+        sys.stdout.write(seq)
+    else:
+        sys.stdout.write(text)
